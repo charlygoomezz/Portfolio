9b5e544820e8a28f6bb12ac3eaea223022a51554
Carlos Gómez <carlosgomzp21@gmail.com>
2026-02-04 16:11:28 +0100

    Create Sidebar component

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -7,9 +7,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import Sidebar from './components/Sidebar.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="lg:flex lg:justify-center lg:items-start lg:gap-10">
+      <Sidebar />
+
+      <App />
+    </div>
   </StrictMode>
 );
