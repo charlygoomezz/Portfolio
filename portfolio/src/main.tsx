f27cb344ad54c67147081eae829eb7080f225833
Carlos Gómez <carlosgomzp21@gmail.com>
2026-02-04 16:12:34 +0100

    Create FloatingMenu.tsx

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -8,11 +8,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import Sidebar from './components/Sidebar.tsx';
+import FloatingMenu from './components/FloatingMenu.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="lg:flex lg:justify-center lg:items-start lg:gap-10">
       <Sidebar />
+      <FloatingMenu />
 
       <App />
     </div>
