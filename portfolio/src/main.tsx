6e8402a2ea12f668459f5ed5c2989d374cbab37f
Carlos Gómez <carlosgomzp21@gmail.com>
2026-02-04 15:54:57 +0100

    create LICENSE

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/**
+ * @copyright 2025 Carlos Gómez Prosper
+ * @license Apache-2.0
+ */
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
